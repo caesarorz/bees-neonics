dcc9e5aa27493729f76652fc8b2a0b2aae2c9949
Cesar Orozco <caesar.orz@gmail.com>
2022-01-03 10:58:30 -0600

    Add navbar, generate dataset for plot2, add style to website

--- static_site/script.js
+++ static_site/script.js
@@ -1,5 +1,7 @@
 url = 'https://raw.githubusercontent.com/caesarorz/bees-neonics/main/data/dataset.json'
 
+config = {response: true}
+
 fetch(url)
   .then((response) => {
     return response.json();
@@ -100,3 +102,52 @@ document.getElementById('filter-neonic').addEventListener("change", function(e){
 });
 
 
+
+var trace1 = {
+  x: [0, 1, 2],
+  y: [10, 11, 12],
+  type: 'scatter'
+};
+
+var trace2 = {
+  x: [2, 3, 4],
+  y: [100, 110, 120],
+  xaxis: 'x2',
+  yaxis: 'y2',
+  type: 'scatter'
+};
+
+var trace3 = {
+  x: [3, 4, 5],
+  y: [1000, 1100, 1200],
+  xaxis: 'x3',
+  yaxis: 'y3',
+  type: 'scatter'
+};
+
+var data = [trace1, trace2, trace3];
+
+var layout = {
+grid: {
+    rows: 3,
+    columns: 1,
+    pattern: 'independent',
+    roworder: 'bottom to top'}
+};
+
+Plotly.newPlot('multi-line-state', data, layout);
+
+
+var data = [
+  {
+      type: "pie",
+      values: [2, 5, 3, 2.5],
+      labels: ["R", "Python", "Java Script", "Matlab"],
+      texttemplate: "%{label}: %{value} (%{percent})",
+      textposition: "inside"
+  }
+];
+
+Plotly.newPlot("subplots-state-pie", data, config)
+
+// subplots-state-pie
